86e1f80b14951a06f1d44355f1d47fa8033f19bd
lladinog <159490210+lladinog@users.noreply.github.com>
2024-03-05 02:57:22 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,11 +1,15 @@
-from  classroom.asignatura import Asignatura
+from classroom.asignatura import Asignatura
 class Grupo:
-    grado = "Grado 12"
+    grado = "Grado 6"
 
-    def __init__(self, grupo="grupo predeterminado", asignaturas=None, listadoAlumnos=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=[], listadoAlumnos=[]):
         self._grupo = grupo
 <<<<<<< HEAD
+<<<<<<< HEAD
         self._asignaturas = asignaturas if asignaturas is not None else []
+=======
+        self._asignaturas = asignaturas
+>>>>>>> parent of 1c25ae1 (Update grupo.py)
         self.listadoAlumnos = listadoAlumnos if listadoAlumnos is not None else []
 =======
         self._asignaturas = asignaturas
@@ -27,4 +31,25 @@ class Grupo:
     def asignarNombre(nombre="Grado 6"):
         Grupo.grado = nombre
 
+class Grupo:
+    grado = "Grado 12"
 
+    def __init__(self, grupo="grupo predeterminado", asignaturas=[], listadoAlumnos=[]):
+        self._grupo = grupo
+        self._asignaturas = asignaturas
+        self.listadoAlumnos = listadoAlumnos
+
+    def __str__(self):
+        return f"Grupo de estudiantes: {self._grupo}"
+
+    def agregarAlumno(self, alumno, alumnos=[]):
+        self.listadoAlumnos.append(alumno)
+        self.listadoAlumnos.extend(alumnos)
+
+    def listadoAsignaturas(self, **kwargs):
+        for key, value in kwargs.items():
+            self._asignaturas.append(Asignatura(value))
+
+    @staticmethod
+    def asignarNombre(nombre="Grado 6"):
+        Grupo.grado = nombre
