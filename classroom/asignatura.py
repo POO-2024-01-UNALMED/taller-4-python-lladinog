1d35048576d9a8025ac4bf343864467e6b47054f
lladinog <159490210+lladinog@users.noreply.github.com>
2024-03-05 01:56:49 -0500

    First Try :)

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,8 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon):
+    def __init__(self, nombre, salon=None):
         self._nombre = nombre
-        self._salon = salon
+        self._salon = salon if salon else "Remoto"
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+         return f"{self._nombre} {self._salon}" if self._salon else f"{self._nombre}"
